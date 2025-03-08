4bc19688fcab6e9f2d90c14654a07e25b22e10c0
Jeet Prakash <11135910+jeetprksh@users.noreply.github.com>
2025-03-08 23:33:21 +0530

    Added CORS config

--- server/src/main/java/com/dupefinder/controller/DuplicateFinderController.java
+++ server/src/main/java/com/dupefinder/controller/DuplicateFinderController.java
@@ -5,10 +5,12 @@ import com.dupefinder.service.DuplicateFileFinder;
 import com.dupefinder.service.DuplicateFileFinderFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController()
 public class DuplicateFinderController {
 
